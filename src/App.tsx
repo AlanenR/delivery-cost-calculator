44658c8957cabff25bf23fdf0f07a12928d13493
Riikka Alanen <riikka.alanen@kuubi.fi>
2024-06-03 10:37:42 +0300

    Add calculator container

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import DeliveryFeeCalculator from "./containers/DeliveryFeeCalculator";
+
+function App() {
+  return (
+    <div className="App">
+      <DeliveryFeeCalculator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/DeliveryFeeCalculator.tsx
@@ -0,0 +1,10 @@
+const DeliveryFeeCalculator = () => 
+{
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default DeliveryFeeCalculator;
